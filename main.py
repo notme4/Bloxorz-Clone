ec7cd4fa14598dc9ec67dc968bff1765acc0d190
notme4 <102337330+notme4@users.noreply.github.com>
2023-10-11 14:07:57 -0500

    add drawCube() and basic movement

--- main.py
+++ main.py
@@ -3,13 +3,28 @@ from pygame import Color, Surface, Vector2
 
 import block.block as B
 
+# COLORS
+WHITE = Color(255, 255, 255)
+LIGHTGRAY = Color(160, 160, 160)
+GRAY = Color(64, 64, 64)
+BLACK = Color(0, 0, 0)
+RED = Color(255, 0, 0)
+YELLOW = Color(255, 255, 0)
+GREEN = Color(0, 255, 0)
+CYAN = Color(0, 255, 255)
+BLUE = Color(0, 0, 255)
 
-def drawIsometric(
+MOVEMENT_DELAY = 0.5
+
+
+def drawIsometricTop(
     surface: Surface,
     color: Color,
-    center: Vector2 = Vector2(0, 0),
+    center: Vector2,
     width: int = 100,
     height: int = 50,
+    outline: bool = False,
+    outlineColor: Color = BLACK,
 ) -> None:
     p1 = Vector2(center.x - width / 2, center.y)
     p2 = Vector2(center.x, center.y + height / 2)
@@ -17,18 +32,92 @@ def drawIsometric(
     p4 = Vector2(center.x, center.y - height / 2)
     coordinates = [p1, p2, p3, p4]
     pygame.draw.polygon(surface, color, coordinates)
+    if outline:
+        pygame.draw.line(surface, outlineColor, p1, p2)
+        pygame.draw.line(surface, outlineColor, p3, p2)
+        pygame.draw.line(surface, outlineColor, p3, p4)
+        pygame.draw.line(surface, outlineColor, p1, p4)
 
 
-# COLORS
-WHITE = Color(255, 255, 255)
-LIGHTGRAY = Color(160, 160, 160)
-GRAY = Color(100, 100, 100)
-BLACK = Color(0, 0, 0)
-RED = Color(255, 0, 0)
-YELLOW = Color(255, 255, 0)
-GREEN = Color(0, 255, 0)
-CYAN = Color(0, 255, 255)
-BLUE = Color(0, 0, 255)
+def drawIsometricRight(
+    surface: Surface,
+    color: Color,
+    center: Vector2,
+    width: int = 100,
+    height: int = 50,
+    outline: bool = False,
+    outlineColor: Color = BLACK,
+) -> None:
+    p1 = Vector2(center.x - width / 4, center.y + height / 4 - height / 2)
+    p2 = Vector2(center.x - width / 4, center.y + height / 4 + height / 2)
+    p3 = Vector2(center.x + width / 4, center.y - height / 4 + height / 2)
+    p4 = Vector2(center.x + width / 4, center.y - height / 4 - height / 2)
+    coordinates = [p1, p2, p3, p4]
+    pygame.draw.polygon(surface, color, coordinates)
+    if outline:
+        pygame.draw.line(surface, outlineColor, p1, p2)
+        pygame.draw.line(surface, outlineColor, p3, p2)
+        pygame.draw.line(surface, outlineColor, p3, p4)
+        pygame.draw.line(surface, outlineColor, p1, p4)
+
+
+def drawIsometricLeft(
+    surface: Surface,
+    color: Color,
+    center: Vector2,
+    width: int = 100,
+    height: int = 50,
+    outline: bool = False,
+    outlineColor: Color = BLACK,
+) -> None:
+    p1 = Vector2(center.x + width / 4, center.y + height / 4 - height / 2)
+    p2 = Vector2(center.x + width / 4, center.y + height / 4 + height / 2)
+    p3 = Vector2(center.x - width / 4, center.y - height / 4 + height / 2)
+    p4 = Vector2(center.x - width / 4, center.y - height / 4 - height / 2)
+    coordinates = [p1, p2, p3, p4]
+    pygame.draw.polygon(surface, color, coordinates)
+    if outline:
+        pygame.draw.line(surface, outlineColor, p1, p2)
+        pygame.draw.line(surface, outlineColor, p3, p2)
+        pygame.draw.line(surface, outlineColor, p3, p4)
+        pygame.draw.line(surface, outlineColor, p1, p4)
+
+
+def drawCube(
+    surface: Surface,
+    color: Color,
+    center: Vector2,
+    sideLength: int,
+    outline: bool = True,
+    outlineColor: Color = BLACK,
+) -> None:
+    drawIsometricTop(
+        surface=surface,
+        color=color,
+        center=Vector2(center.x, center.y - sideLength / 2),
+        width=sideLength * 2,
+        height=sideLength,
+        outline=outline,
+        outlineColor=outlineColor,
+    )
+    drawIsometricRight(
+        surface=surface,
+        color=color,
+        center=Vector2(center.x + sideLength / 2, center.y + sideLength / 4),
+        width=sideLength * 2,
+        height=sideLength,
+        outline=outline,
+        outlineColor=outlineColor,
+    )
+    drawIsometricLeft(
+        surface=surface,
+        color=color,
+        center=Vector2(center.x - sideLength / 2, center.y + sideLength / 4),
+        width=sideLength * 2,
+        height=sideLength,
+        outline=outline,
+        outlineColor=outlineColor,
+    )
 
 
 def main():
@@ -41,7 +130,8 @@ def main():
     running = True
     dt = 0
     # load level
-
+    cubePos = Vector2(100, 100)
+    movementTimeout = 0
     # game loop
     while running:
         #    check for user input
@@ -53,13 +143,28 @@ def main():
         if keys[pygame.K_ESCAPE]:
             running = False
 
+        if movementTimeout < 0:
+            if keys[pygame.K_d]:
+                cubePos += Vector2(50, 25)
+                movementTimeout = MOVEMENT_DELAY
+            if keys[pygame.K_a]:
+                cubePos -= Vector2(50, 25)
+                movementTimeout = MOVEMENT_DELAY
+
+            if keys[pygame.K_s]:
+                cubePos -= Vector2(50, -25)
+                movementTimeout = MOVEMENT_DELAY
+            if keys[pygame.K_w]:
+                cubePos += Vector2(50, -25)
+                movementTimeout = MOVEMENT_DELAY
         #    check win/fail conditions
 
         #    animate
-        screen.fill(BLACK)
-        drawIsometric(screen, LIGHTGRAY, Vector2(100, 100))
-        drawIsometric(screen, GRAY, Vector2(150, 125))
+        screen.fill(GRAY)
+        drawCube(screen, RED, cubePos, 50)
         pygame.display.flip()
+
+        movementTimeout -= dt
 
         dt = clock.tick(60) / 1000
     pygame.quit()
