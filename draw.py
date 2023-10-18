be52876e22d6fa9b9d9e055b6210e04a32966cea
notme4 <102337330+notme4@users.noreply.github.com>
2023-10-18 02:15:12 -0500

    change colors to panda3d

--- draw.py
+++ draw.py
@@ -1,11 +1,12 @@
-_TODO = None
+from panda3d.core import Vec4
+
 # COLORS
-WHITE = (255, 255, 255)
-LIGHTGRAY = (160, 160, 160)
-GRAY = (64, 64, 64)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-YELLOW = (255, 255, 0)
-GREEN = (0, 255, 0)
-CYAN = (0, 255, 255)
-BLUE = (0, 0, 255)
+WHITE = Vec4(255, 255, 255, 1.0)
+LIGHTGRAY = Vec4(160, 160, 160, 1.0)
+GRAY = Vec4(64, 64, 64, 1.0)
+BLACK = Vec4(0, 0, 0, 1.0)
+RED = Vec4(255, 0, 0, 1.0)
+YELLOW = Vec4(255, 255, 0, 1.0)
+GREEN = Vec4(0, 255, 0, 1.0)
+CYAN = Vec4(0, 255, 255, 1.0)
+BLUE = Vec4(0, 0, 255, 1.0)
